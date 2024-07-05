f4a0a78ad02690c9217031bd33d5f60343d79f81
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-07-05 23:11:05 +0300

    'Deploy

---
+++ assets/FavoritesPage-BApHRV4y.js
@@ -0,0 +1,1 @@
+import{u as r,j as s}from"./index-0gq4PnvN.js";import{a as t,C as a}from"./CamperList-xIJtrIM1.js";function m(){const e=r(t);return s.jsx("div",{children:s.jsx(a,{campers:e})})}export{m as default};
