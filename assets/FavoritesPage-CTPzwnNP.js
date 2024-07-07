f3b498fe7d6da3e9f05adfa9e4541421e86ad5e8
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-07-07 16:40:07 +0300

    Updates

--- assets/FavoritesPage-CTPzwnNP.js
+++
@@ -1,1 +0,0 @@
-import{a as t,u as r,j as e,M as o}from"./index-D5Vv_vfU.js";import{d as i,C as n}from"./CamperList-CFOVvuET.js";function m(){const s=t(i),a=r();return s.length>0?e.jsx(n,{campers:s}):e.jsxs(o,{children:[e.jsx("h2",{children:"No campers were added to favorite"}),e.jsx("button",{onClick:()=>{a("/catalog")},children:"Explore campers now"})]})}export{m as default};
